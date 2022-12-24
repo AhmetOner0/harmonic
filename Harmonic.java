029ae5a2fe72558df2e3b3e2d79745466c6a2035
AhmetOner0 <116954834+AhmetOner0@users.noreply.github.com>
2022-12-24 15:37:29 +0300

    Add files via upload

---
+++ Harmonic.java
@@ -0,0 +1,13 @@
+package patikadev;
+
+public class Harmonic {
+    public static void main(String[] args) {
+        int[] list={1,2,3,4,5};
+        double sum=0.0;
+        for (int i=0;i<list.length;i++){
+            sum += 1.0/list[i];
+        }
+        double average = list.length/ sum;
+        System.out.println(average);
+    }
+}
